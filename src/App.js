605abd81b5dc554a3f9e1cff35a45d9180c9aa8c
fares fakhfakh <faresfakhfakh97@gmail.com>
2021-12-01 17:31:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <section className="hea">
+        <h1>Fares Fakhfakh</h1>
+        <img src="/received_ff.jpeg" alt=""/>
+        <img src="/r.jpg" alt="" />
+      </section>
+        <video src="/free.mp4" controls ></video>
+    </div>
+  );
+}
+
+export default App;
