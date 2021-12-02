d5baec99ae3a192b9ee31d69115161746d887a30
fares fakhfakh <faresfakhfakh97@gmail.com>
2021-12-02 10:04:15 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import image from "./image.jpg";
+import video from "./free.mp4"
 function App() {
   return (
     <div className="App">
       <section className="hea">
         <h1>Fares Fakhfakh</h1>
         <img src="/received_ff.jpeg" alt=""/>
-        <img src="/r.jpg" alt="" />
+        <img src={image} alt="" />
       </section>
-        <video src="/free.mp4" controls ></video>
+        <video src={video} controls ></video>
     </div>
   );
 }
